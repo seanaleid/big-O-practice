a0cb312f4e1090cba887760c884ef47c8bd8e915
seanaleid <seanaleid@gmail.com>
2020-07-13 13:32:12 -0400

    Create new files for linked lists notes

--- big-0.py
+++ big-0.py
@@ -4,22 +4,40 @@ O(n) - Linear Time
 animals = ['Duck', 'Jackal', 'Hippo', 'Aardvark', 'Cat',
            'Flamingo', 'Iguana', 'Giraffe', 'Elephant', 'Bear']
 ​
+​""" 
+O(1) - Constant Time
+"""
+
+# def print_animals(animal_list):
+#     print(animal_list[0])
+
+# def print_animals(animal_list):
+#     for n in range(10000)
+#         print(animal_list[0])
 ​
+""" 
+O(n) - Linear Time
+"""
 def print_animals(animal_list):
-    print(animal_list[0])
-​
+    for n in range(len(animal_list))
+        print(animal_list[0])
 ​
 """
 What about this one? What runtime complexity is this one?
 """
-​
-​
-def print_animals(animal_list):
-    for i in range(len(animal_list)):
-        print(animal_list[i])
-        my_number = 0
-        for _ in range(100000):
-            my_number += 1
+
+""" 
+O(n^2) - Quadratic Time
+"""​
+def print_animals(animal_list): # This line is 0(n * (1 + 1 + 1 + (100000 * 1))) = O(n * 10003) = O(100003n) = O(n)
+    for i in range(len(animal_list)): # This line is 0(n) - Linear
+        print(animal_list[i]) # This line is 0(1) - Constant
+        print(animal_list[i]) # This line is 0(1) - Constant --> (1 + 1) * n 
+        # O(1+1) = ) = (2)
+
+        my_number = 0 # This line is 0(1) - Constant
+        for _ in range(100000): # This line is 0(100000) - Constant
+            my_number += 1 # This line is 0(1) - Constant
 ​
 ​
 """
@@ -35,15 +53,15 @@ animals = ['Duck', 'Jackal', 'Hippo', 'Aardvark', 'Cat',
 ​
 ​
 # Print a list of all possible animal pairs
-def print_animal_pairs():
-    for animal_1 in animals:
-        for animal_2 in animals:
-            print(f"{animal_1} - {animal_2}")
+def print_animal_pairs(lst): ) #O(n*(n*1)) = O(n*n) = O(n^2)
+    for animal_1 in lst: # O(n)
+        for animal_2 in lst: # O(n)
+            print(f"{animal_1} - {animal_2}") # O(1)
 ​
 ​
-# Print a list of all possible animal triples
+# Print a list of all possible animal triples O(n^3)
 def print_animal_triples():
-    for animal_1 in animals:
+    for animal_1 in animals: # O(n)
         for animal_2 in animals:
             for animal_3 in animals:
                 print(f"{animal_1} - {animal_2} - {animal_3}")
@@ -87,4 +105,14 @@ def free_animals(animal_list):
 CFU: Slack Thread
 What's the worst the number of steps can get for a O(log n) time complexity
 algorithm with an input size of 10 million?
+"""
+
+"""
+-Linked Lists have O(1) appends
+-Linked Lists have O(1) prepends
+-Linked Lists have O(n) lookups
+
+-Dynamic Arrays have O(n) appends
+-Dynamic Arrays have O(n) prepends
+-Dynamic Arrays have O(1) lookups
 """
